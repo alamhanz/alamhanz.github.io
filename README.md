98f3b5d2f77b9d53a62f9b6d97c1f9ad679524ef
alamhanz <alamhanz@gmail.com>
2022-10-02 06:41:50 +0700

    xyz file

--- README.md
+++
@@ -1,7 +0,0 @@
-# Personal Web
-My Personal Branding Web Testing
-
-## Credits
-
-* [Forty Template](https://html5up.net/forty)
-* [Sharan article](https://medium.com/towards-data-science/how-to-create-a-stunning-personal-portfolio-website-for-free-50ec15b059dd)
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./*.html"],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        'body': '#F5F5F5',
+        'selected-text': '#F05454',
+        'theme': '#30475E',
+        'button' : '#121212'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
